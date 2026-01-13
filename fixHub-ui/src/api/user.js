dec588f8e3751ac2c550c83dadc94e78b4e243ed
most3 <3162313956@qq.com>
2026-01-13 11:17:48 +0800

    修复管理员创建账号

--- fixHub-ui/src/api/user.js
+++ fixHub-ui/src/api/user.js
@@ -1,5 +1,6 @@
 import client from './client'
 
+// 管理员创建用户接口：指向后端 API 路径 /api/users
 export const createUser = (data) => {
-  return client.post('/users', data)
+  return client.post('/api/users', data)
 }
